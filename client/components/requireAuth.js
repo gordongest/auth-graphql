4aa2be3f83cd32ee9fc7007c1625f1a9640d93a9
Gordon Gest <gordon.gest@gmail.com>
2022-10-18 16:09:48 -0600

    course complete — final commit

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { hashHistory } from 'react-router';
 import { graphql } from 'react-apollo';
 import fetchCurrentUser from '../queries/fetchCurrentUser';
